1562a4b4445c884428f14bb0349274c173b754a0
Eduardo Andrade <eduardoandrade@MacBook-Air-de-Eduardo.local>
2024-01-17 18:07:57 -0500

    Opciones del menu y base del programa creada

---
+++ main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int opc;
+
+    do{
+        printf("1. Ingresar datos de los alumnos\n");
+        printf("2. Imprimir los datos de estudiantes\n");
+
+        
+    } while (opc!= 1 || opc != 2);
+    return 0;
+}

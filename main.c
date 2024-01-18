51c019a4602189469942a28609460ab9533a2040
Eduardo Andrade <eduardoandrade@MacBook-Air-de-Eduardo.local>
2024-01-17 21:22:32 -0500

    Mas documentacion agregada

--- main.c
+++ main.c
@@ -32,7 +32,7 @@ int main(int argc, char const *argv[]) {
             case 1: {
                 printf("Ingrese el numero de alumnos de los cuales desea ingresar datos\n"); //Se solicita al usuario el numero de alumnos a analizar 
                 scanf("%d", &n);
-                fflush(stdin);
+                fflush(stdin); // Esta funcion permite limpiar el espacio de memoria luego de haber ingresado un dato, para evitar que se lean datos ingresados anteriormente
                 for (i = 0; i < n; i++) { //La estructura for permite asignar los datos de cada alumno hasta que se llenen todos los campos de todos los alumno
                     printf("\nDatos del alumno %d\n", i + 1);
                     printf("Ingrese el precio de matricula: "); //En estas lineas de codigo dentro del for, preguntamos los datos y se le asigna a cada valor de la estructura
@@ -64,7 +64,7 @@ int main(int argc, char const *argv[]) {
             case 3: {
                 puts ("Saliendo\n"); // Imprime Saliendo si ya no quiere realizar mas operaciones el usuario 
             }
-            default: {
+            default: { //Muestra lo que esta dentro del puts si ingresa una opcion que no esta dentro del menu
                 if (opc>3) {
                     puts("La opcion ingresada no es correcta\n");
                 }
